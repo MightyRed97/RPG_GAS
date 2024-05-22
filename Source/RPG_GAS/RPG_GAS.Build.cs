1d04d708d28f0a49e40e00293d1a0673a70d17b7
MightyRed97 <mightyred0907@gmail.com>
2024-05-23 00:33:37 +0800

    Init GAS dependency

--- Source/RPG_GAS/RPG_GAS.Build.cs
+++ Source/RPG_GAS/RPG_GAS.Build.cs
@@ -8,8 +8,6 @@ public class RPG_GAS : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
-
-		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+		PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks", "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
 	}
 }
