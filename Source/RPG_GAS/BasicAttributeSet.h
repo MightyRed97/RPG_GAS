c689b262c829fa5e2526675389db39937187c5a4
MightyRed97 <mightyred0907@gmail.com>
2024-05-23 01:31:40 +0800

    Init GAS plugin in ue editor

---
+++ Source/RPG_GAS/BasicAttributeSet.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "BasicAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName)\
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName)\
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName)\
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName)\
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_GAS_API UBasicAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UBasicAttributeSet, Health);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UBasicAttributeSet, Mana);
+};
+
